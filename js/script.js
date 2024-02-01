ac591cb0e5fee0f8cdb1ea007da1c5113e1f2b7c
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-01-31 23:13:30 -0300

    problema na api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
-const endpoint = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e9d7d8e62d3d6b589222dfdc4648be38"
+let lat = "30.489772"
+let lon = -"99.771335"
+const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={e9d7d8e62d3d6b589222dfdc4648be38}`
 
 fetch(endpoint)
 .then(dados => dados.json())
