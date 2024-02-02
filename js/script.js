1ba84aa128a84f4ca9fa92cf244d095814ada37b
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-02-01 23:47:56 -0300

    fix and add: bug na api e manipulando json

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+const input = document.getElementById("input");
+const botaoPesquisar = document.getElementById("botao-pesquisar");
 
-let lat = "30.489772"
-let lon = -"99.771335"
-const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={e9d7d8e62d3d6b589222dfdc4648be38}`
 
-fetch(endpoint)
-.then(dados => dados.json())
-.then(dados => console.log(dados))
+botaoPesquisar.onclick = () =>{
+    pesquisaCidade(input.value)
+}
+
+const buscaInfos = (param1, param2) => {
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${param1}&lon=${param2}&appid=e9d7d8e62d3d6b589222dfdc4648be38`;
+    fetch(endpoint)
+    .then(dados => dados.json())
+    .then(dados => console.log(dados))
+}
+
+const pesquisaCidade = (param) => {
+    const endpoint2 = `https://api.openweathermap.org/geo/1.0/direct?q=${param}&appid=e9d7d8e62d3d6b589222dfdc4648be38`
+    fetch(endpoint2)
+    .then(dados => dados.json())
+    .then(dados => {
+        buscaInfos(dados[0].lat, dados[0].lon);
+    })
+
+    
+}
