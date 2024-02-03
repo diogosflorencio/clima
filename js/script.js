6aecf145e1045e8b5f5f0045bd132abdd3496db2
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-02-02 22:52:57 -0300

    add: pesquisar clicando no Enter

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ const botaoPesquisar = document.getElementById("botao-pesquisar");
 const telaConteudo = document.getElementsByClassName("tela-conteudo")[0]
 
 botaoPesquisar.onclick = () =>{
-    pesquisaCidade(input.value)
+    pesquisaCidade(input.value);
 }
+input.addEventListener("keydown", (event) => {
+    if(event.key == "Enter"){
+        pesquisaCidade(input.value);
+    }
+})
 
 const buscaInfos = (param1, param2) => {
     const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${param1}&lon=${param2}&appid=e9d7d8e62d3d6b589222dfdc4648be38`;
